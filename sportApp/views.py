addc3e24adfafcf5a5f69c9b98be05f8be58a766
StivenPabloJimenezCastillo <134020456+StivenPabloJimenezCastillo@users.noreply.github.com>
2023-07-25 00:50:20 -0500

    Update views.py

--- sportApp/views.py
+++ sportApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def futbol(request):
+    return render(request, 'futbol.html')
+
+def basquetbol(request):
+    return render(request, 'basquetbol.html')
+
+def ajedrez(request):
+    return render(request, 'ajedrez.html')
+
+def pingpong(request):
+    return render(request, 'pingpong.html')
